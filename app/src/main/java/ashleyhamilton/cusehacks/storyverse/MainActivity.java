91c1d795a01105a48812523c102b3680c736f5d7
Ashley Hamilton <91693425+amhamiltsyr@users.noreply.github.com>
2022-02-19 19:34:37 -0500

    start of saving

--- app/src/main/java/ashleyhamilton/cusehacks/storyverse/MainActivity.java
+++ app/src/main/java/ashleyhamilton/cusehacks/storyverse/MainActivity.java
@@ -17,55 +17,62 @@ import java.util.Dictionary;
 
 
 public class MainActivity extends AppCompatActivity {
-    private int yearGoalBooks;
+    private String yearGoalBooks;
     private int yearProgressBooks;
     private int yearProgressMovies;
     private int yearProgressTV;
-    private int yearGoalMovies;
-    private int yearGoalTV;
-    private String[][] total;
-    private int currentIndexTotal;
-    private String[][] favorite;
-    private int currentIndexFavorite;
+    private String yearGoalMovies;
+    private String yearGoalTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null){
-            yearGoalBooks=0;
+            yearGoalBooks="0";
             yearProgressBooks=0;
             yearProgressMovies=0;
             yearProgressTV=0;
-            yearGoalMovies=0;
-            yearGoalTV=0;
-            favorite= new String[100][3];
-            total=new String[100][3];
+            yearGoalMovies="0";
+            yearGoalTV="0";
         }
         Intent intent = getIntent();
-        if(intent.getExtras()!=null){
+        Bundle extras = intent.getExtras();
+        if(extras!=null){
+            if(extras.containsKey("yearGoalBooks_key")){
+                yearGoalBooks=intent.getStringExtra("yearGoalBooks_key");
+            }
+            if(extras.containsKey("yearGoalTV_key")){
+                yearGoalTV=intent.getStringExtra("yearGoalTV_key");
+            }
+            if(extras.containsKey("yearGoalMovies_key")){
+                yearGoalMovies=intent.getStringExtra("yearGoalMovies_key");
+            }
+            if(extras.containsKey("yearProgressBooks_key")){
+                yearProgressBooks=Integer.parseInt(intent.getStringExtra("yearProgressBooks_key"));
+            }
+            if(extras.containsKey("yearProgressTV_key")){
+                yearProgressTV=Integer.parseInt(intent.getStringExtra("yearProgressTV_key"));
+            }
+            if(extras.containsKey("yearProgressMovies_key")){
+                yearProgressMovies=Integer.parseInt(intent.getStringExtra("yearProgressMovies_key"));
+            }
             System.out.println("happening");
-            if(intent.getStringExtra("favor_key").equals("1")) {
-                // is a favorite
-                favorite[currentIndexFavorite][0] = intent.getStringExtra("saveName_key");
-                favorite[currentIndexFavorite][1] = intent.getStringExtra("content_key");
-                currentIndexFavorite++;
-                total[currentIndexTotal][0] = intent.getStringExtra("saveName_key");
-                total[currentIndexTotal][1] = intent.getStringExtra("content_key");
-                currentIndexTotal++;
+            if(extras.containsKey("content_key")) {
+                if (intent.getStringExtra("content_key").equals("Book")) {
+                    yearProgressBooks++;
+                } else if (intent.getStringExtra("content_key").equals("Movie")) {
+                    yearProgressMovies++;
+                } else if (intent.getStringExtra("content_key").equals("TV")) {
+                    yearProgressTV++;
+                }
             }
-            else if(intent.getStringExtra("favor_key").equals("")){
-                //not a favorite
-                total[currentIndexTotal][0]=intent.getStringExtra("saveName_key");
-                total[currentIndexTotal][1]=intent.getStringExtra("content_key");
-                currentIndexTotal++;
+            if(extras.containsKey("bookGoal_key")){
+                yearGoalBooks=intent.getStringExtra("bookGoal_key");
             }
-            if(intent.getStringExtra("content_key").equals("Book")){
-                yearProgressBooks++;
+            if(extras.containsKey("movieGoal_key")){
+                yearGoalMovies=intent.getStringExtra("movieGoal_key");
             }
-            else if(intent.getStringExtra("content_key").equals("Movie")){
-                yearProgressMovies++;
-            }
-            if(intent.getStringExtra("content_key").equals("TV")){
-                yearProgressTV++;
+            if(extras.containsKey("tvGoal_key")){
+                yearGoalTV=intent.getStringExtra("tvGoal_key");
             }
         }
         setContentView(R.layout.activity_main);
@@ -73,38 +80,32 @@ public class MainActivity extends AppCompatActivity {
         String toSet="Yearly Goal:\nBooks: "+yearProgressBooks+"/"+yearGoalBooks+"\nMovies: "+yearProgressMovies+"/"+yearGoalMovies+"\nTV: "+yearProgressTV+"/"+yearGoalTV+"\n";
         yearGoalTracker.setText(toSet);
         Button add=findViewById(R.id.add);
-        ImageButton favorites=findViewById(R.id.favorites);
-        ImageButton year=findViewById(R.id.year);
-        ImageButton account=findViewById(R.id.account);
         Button setYearGoal=findViewById(R.id.setYearGoal);
         View.OnClickListener myListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v.getId() == R.id.add) {
                     Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                    intent.putExtra("yearGoalBooks_key", yearGoalBooks);
+                    intent.putExtra("yearGoalMovies_key", yearGoalMovies);
+                    intent.putExtra("yearGoalTV_key", yearGoalTV);
+                    intent.putExtra("yearProgressBooks_key", yearProgressBooks);
+                    intent.putExtra("yearProgressBooks_key", yearProgressMovies);
+                    intent.putExtra("yearProgressBooks_key", yearProgressTV);
                     startActivity(intent);
                 } else if (v.getId() == R.id.setYearGoal) {
                     Intent intent = new Intent(MainActivity.this, setYearGoalActivity.class);
-                    startActivity(intent);
-                } else if (v.getId() == R.id.favorites) {
-                    Intent intent = new Intent(MainActivity.this, favoritesActivity.class);
-                    startActivity(intent);
-                } else if (v.getId() == R.id.year) {
-                    Intent intent = new Intent(MainActivity.this, yearActivity.class);
-                    startActivity(intent);
-                }
-                else if(v.getId()==R.id.account){
-                    Intent intent = new Intent(MainActivity.this, accountActivity.class);
+                    intent.putExtra("yearGoalBooks_key", yearGoalBooks);
+                    intent.putExtra("yearGoalMovies_key", yearGoalMovies);
+                    intent.putExtra("yearGoalTV_key", yearGoalTV);
+                    intent.putExtra("yearProgressBooks_key", yearProgressBooks);
+                    intent.putExtra("yearProgressBooks_key", yearProgressMovies);
+                    intent.putExtra("yearProgressBooks_key", yearProgressTV);
                     startActivity(intent);
                 }
             }
         };
         add.setOnClickListener(myListener);
         setYearGoal.setOnClickListener(myListener);
-        favorites.setOnClickListener(myListener);
-        year.setOnClickListener(myListener);
-        account.setOnClickListener(myListener);
-
-
     }
 }
